88adcb3b2c180ac2a62d23f0be8c99b6b74fffea
Jevon Cochran <jevon.cochran@gmail.com>
2021-04-18 10:35:50 -0700

    first commit

---
+++ client/src/config/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBd3NzCD0Nc9TmaEqXoVmeTWfCn60LRzHM",
+  authDomain: "fir-auth-617bb.firebaseapp.com",
+  projectId: "fir-auth-617bb",
+  storageBucket: "fir-auth-617bb.appspot.com",
+  messagingSenderId: "577870379591",
+  appId: "1:577870379591:web:77815d2c33f0fc7427cf62",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
---
+++ client/src/service/auth.js
@@ -0,0 +1,15 @@
+import firebase from "../config/firebaseConfig";
+
+const platformAuth = (provider) => {
+  return firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((res) => {
+      return res.user;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+export default platformAuth;
